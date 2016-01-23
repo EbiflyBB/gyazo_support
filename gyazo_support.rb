5e687f4d825febab2ec6b8ed279f42ae922c5d75
EbiflyBB <ebiflybb@gmail.com>
2016-01-24 01:55:12 +0900

    specファイル追加しました

--- gyazo_support.rb
+++ gyazo_support.rb
@@ -1,6 +1,5 @@
 # coding: utf-8
 Plugin.create :photo_support_gyazo do
-  # gyazoがなんか展開できないのもあるらしい
   defimageopener('gyazo', %r<\Ahttps?://gyazo.com/[a-zA-Z0-9]+>) do |display_url|
     connection = HTTPClient.new
     page = connection.get_content(display_url)
